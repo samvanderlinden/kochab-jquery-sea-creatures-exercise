41e8bf63adf836d49c5c4571edb51212625e3c48
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-13 15:11:29 -0500

    allow user to add new creature

--- client.js
+++ client.js
@@ -1,7 +1,35 @@
 console.log('JS sourced');
 
+let clickCount = 0;
+
 $(document).ready(onReady);
 
 function onReady (){
-    console.log('jQuery sourced');
+    $('#counterButton').on('click', clickHandler);
+    updateDomCount();
+}
+
+function clickHandler() {
+    console.log('counter clicked');
+    clickCount++;
+    console.log(clickCount);
+    // $('#counter').text('Total count is: ' + clickCount);
+    updateDomCount();
+    addNewCreature();
+}
+
+function updateDomCount () { // we are CALLING this function in the clickHandler function above
+    $('#counter').text(clickCount);
+}
+
+function addNewCreature() {
+    $('#creatureList').append(            
+    
+    '<tr>'  +
+        '<td>' + $('#newCreatureName').val() + '</td>' +
+        '<td>' + $('#newCreatureSize').val() + ' feet' + '</td>' +
+        '<td>' + $('#newCreatureType').val() + '</td>' +
+        '<td>' + $('#newCreatureSpeed').val() + ' mph' + '</td>' +
+    '</tr>'
+    );
 }
